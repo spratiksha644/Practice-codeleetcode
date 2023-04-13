368ce226a179cc9e57bd149216dcb7a3bc32047e
spratiksha644 <119289595+spratiksha644@users.noreply.github.com>
2023-04-14 01:46:18 +0530

    Update and rename NeSEL.cpp to NSER.cpp

--- NSER.cpp
+++ NSER.cpp
@@ -1,18 +1,18 @@
-next smaller element to left: 
-  
+next smaller element to right: 
+          
          class Solution { 
          public: 
-             vector<int> left(vector<int>& arr){ 
+             vector<int> right(vector<int>& nums) { 
                  vector<int> ans; 
                  stack<int> st; 
-                 for(int i = 0; i < arr.size(); i++){ 
-                     while(st.empty() && st.top() >= arr[i]) 
+                 for(int i = nums.size() - 1; i >= 0; i--){ 
+                     while(st.empty() && st.top() >= nums[i]) 
                          st.pop(); 
                      if(st.empty()) 
                          ans.push_back(-1); 
                      else 
                          ans.push_back(st.top()); 
-                     st.push(arr[i]); 
+                     st.push(nums[i]); 
                  } 
                  return ans; 
              } 
