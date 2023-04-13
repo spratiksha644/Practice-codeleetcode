dc30d2b354f268b4892d4d28f06f37622ee48081
spratiksha644 <119289595+spratiksha644@users.noreply.github.com>
2023-04-14 01:49:17 +0530

    Create Valid-parenthesis.cpp

---
+++ Valid-parenthesis.cpp
@@ -0,0 +1,19 @@
+Valid parnetheses: 
+  
+         class Solution { 
+         public: 
+             bool isValid(string str){ 
+                 stack<char> st; 
+                 for(auto ch : str) { 
+                     if(ch == '(' || ch == '{' || ch == '[') 
+                         st.push(ch); 
+                     else 
+                         if(st.empty() || (ch == ')' && st.top() != '(') ||  
+                         (ch == '}' && st.top() != '{') || (ch == ']' && st.top() != '[')) 
+                             return false; 
+                         st.pop(); 
+                     } 
+                 } 
+                 return st.empty(); 
+             } 
+         };
